01f0e853f6773b7ae0ea5de76a56402560da860b
Alex Aragon <Alex.Aragon@kingcounty.gov>
2017-02-28 14:55:40 -0800

    adding socrata pkg, Move routes to own file

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,17 +1,81 @@
-var express = require('express');
+var router = require('express').Router();
+var config = require('../config/config.json');
+var soda = require('soda-js');
+var sodaOpts = config.socrata;
+var producer = new soda.Producer('data.kingcounty.gov', sodaOpts);
 
-var routes = function(){
-    var router = express.Router();
-    router.route('/')
-        .get(function(req,res){
-            console.log('hello');
-            res.render('index')
-        })
-        .post(function (req, res) {
-            console.log(req.body);
-            res.redirect('/');
-        });
-    return router;
+var routes = function () {
+  router.route('/')
+    .get(function (req, res) {
+      res.render('index');
+    })
+    .post(function (req, res) {
+      var body = req.body;
+      body.displayOnHomePage = (body.displayOnHomePage) ? true : false;
+      var data = {
+        start_time: body.startTimeDate,
+        end_time: body.endTimeDate,
+        event_name: body.eventName,
+        event_description_details: body.eventDescription,
+        location_name: body.locationName,
+        location: {
+          human_address: {
+            address:body.street,
+            city:body.city,
+            state: body.state,
+            zip: body.zip
+          }
+        },
+        url: body.linkUrl,
+        contact_name: body.contactName,
+        contact_email: body.contactEmail,
+        contact_phone: body.contactPhoneType,
+        host_contact_department: body.contactDept,
+        feed_keyword_s: body.feedKeywords,
+        home_page_y_n: body.displayOnHomePage,
+        arts_culture: body.categoryArtsCulture,
+        best_starts_for_kids: body.categoryBestStarts,
+        business: '',
+        closure: '',
+        council: '',
+        courts: '',
+        elections: '',
+        emergency: '',
+        employees: '',
+        environment: '',
+        events: '',
+        executive: '',
+        health: '',
+        jails: '',
+        jobs: '',
+        licenses: '',
+        metro: '',
+        news: '',
+        operations: '',
+        parks: '',
+        permits: '',
+        pets: '',
+        property: '',
+        recreation: '',
+        recycling_trash: '',
+        roads: '',
+        rural: '',
+        safety: '',
+        sheriff: '',
+        socialservices: '',
+        transportation: '',
+        volunteer: '',
+        tac_meeting: ''
+      };
+      // Push to Socrata
+      producer.operation()
+        .withDataset('w9jp-gibf')
+        .add(data)
+          .on('success', function(row) { console.log(row);  })
+          .on('error', function(error) { console.error(error); })
+      res.redirect('/');
+    });
+  return router;
 };
 
 module.exports = routes;
--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var config = require('./config/config.json');
-
-var soda = require('soda-js');
-
-var sodaOpts = config.socrata;
-
-var producer = new soda.Producer('data.kingcounty.gov', sodaOpts);
-
 var app = express();
 var port = process.env.PORT || 3000;
 
@@ -15,31 +7,8 @@ app.set('view engine', 'pug');
 app.use(express.static('bower_components'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// var router = require('./Routes/routes');
+app.use('/', require('./routes/routes')());
 
-app.get('/', function(req,res){
-  res.render('index')
-});
-
-app.post('/', function (req, res) {
-  console.log(req.body);
-  var data = {
-    mynum : 42,
-    mytext: "hello world",
-    mymoney: 999.99,
-    contact_name: 'Alex testing'
-  }
-
-  console.log("Adding Sample")
-
-  producer.operation()
-    .withDataset('rnmi-uwsb')
-    .upsert(data)
-      .on('success', function(row) { console.log(row);  })
-      .on('error', function(error) { console.error(error); })
-  res.redirect('/');
-});
-
-app.listen(port, function(){
-    console.log('Running on port: ' + port);
+app.listen(port, function () {
+  console.log('Running on port: ' + port);
 });
