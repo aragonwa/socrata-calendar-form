0ea96ed88f8a45fda80fd8efc0524b0ab8ceee42
Aragon <aragonwa@gmail.com>
2018-03-20 16:23:14 -0700

    WIP

--- app.js
+++ app.js
@@ -8,62 +8,32 @@ OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
 passport.use('Socrata', new OAuth2Strategy({
     authorizationURL: 'https://data.kingcounty.gov/oauth/authorize',
     tokenURL: 'https://data.kingcounty.gov/oauth/access_token',
-    userAuthorizationURL: 'https://kc-calendar.herokuapp.com//oauth/authorize',
+    userAuthorizationURL: 'https://kcalex.localtunnel.me/oauth/authorize',
     clientID: 'HUhrupu6tx5TyuUqQ0RYPcdDm',
     clientSecret: 'uwpxW9vV-8ULqdf823ATu33UN6sLmDmGAWxu',
-    callbackURL: 'https://kc-calendar.herokuapp.com/auth/socrata/callback'
+    callbackURL: 'https://kcalex.localtunnel.me/auth/socrata/callback'
   },
   function(token, tokenSecret, profile, done) {
-    console.log(profile);
+    return done(null, profile)
   }
 ));
-// const Grant = require('grant-express');
-// https://scotch.io/tutorials/implement-oauth-into-your-express-koa-or-hapi-applications-using-grant
-// const grant = new Grant({
-//   "server": {
-//     "protocol": "https",
-//     "host": "alex.kingcounty.gov:3000"
-//   },
-//   "socrata": {
-//     "authorize_url": "https://soda.demo.socrata.com/oauth/authorize",
-//     "access_url": "https://soda.demo.socrata.com/oauth/access_token",
-//     "oauth": 2,
-//     "key": "HUhrupu6tx5TyuUqQ0RYPcdDm",
-//     "secret": "uwpxW9vV-8ULqdf823ATu33UN6sLmDmGAWxu",
-//     "callback": "/handle_socrata_callback",
-//   }
-// });
-
-
 
 const app = express();
 app.use(session({ secret: 'very secret'}));
-// app.use(grant);
-// https://www.npmjs.com/package/express-sslify
-// app.get('/login', function (req, res) {
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(function(user, done) {
+  // placeholder for custom user serialization
+  // null is for errors
+  done(null, user);
+});
 
-//   // https://soda.demo.socrata.com/oauth/authorize?client_id=HUhrupu6tx5TyuUqQ0RYPcdDm&response_type=code &redirect_uri=http:localhost:3000/handle_socrata_callback
-// })
-
-
-// app.get('/handle_socrata_callback', function (req, res) {
-//   console.log(req)
-//   res.end(JSON.stringify(req, null, 2))
-// })
-
-// const oauth2 = require('simple-oauth2').create({
-//   client: {
-//     id: '',
-//     secret: ''
-//   },
-//   auth: {
-//     tokenHost: 'https://soda.demo.socrata.com/'
-//   }
-// }); 
-
-
-// res.redirect(authorizationUri);
-// const session = require('express-session');
+passport.deserializeUser(function(user, done) {
+  // placeholder for custom user deserialization.
+  // maybe you are going to get the user from mongo by id?
+  // null is for errors
+  done(null, user);
+});
 
 if(process.env.NODE_ENV !== 'production') {
   app.use(require('morgan')('dev'));
@@ -73,39 +43,52 @@ const creds = (process.env.NODE_ENV === 'production') ? require('./config/config
 
 const port = process.env.PORT || 3000;
 
-// app.oauth = oauthserver({
-//   model: {},
-//   grants: ['password'],
-//   debug: true
-// });
-
-// app.all('/oauth/token', app.oauth.grant());
-//https://soda.demo.socrata.com/oauth/authorize?client_id=YOUR_AUTH_TOKEN&response_type=code &redirect_uri=YOUR_REDIRECT_URI
-
-// app.use(session({
-//   secret: creds.session.secret,
-//   resave: false, // don't save session if unmodified
-//   saveUninitialized: false, // don't create session until something stored
-// }));
-
-// app.get('/', app.oauth.authorise(), function (req, res) {
-//   res.send('Secret area');
-// });
-
-// app.use(app.oauth.errorHandler());
 
 app.set('view engine', 'pug');
 app.use(express.static('bower_components'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+let code = '';
+//Login
+app.get('/', function (req, res) {
+  if (req.isAuthenticated()) {
+    res.redirect('/allow')
+  }
+  var html = "<ul>\
+    <li><a href='/auth/socrata'>Socrata</a></li>\
+    <li><a href='/logout'>logout</a></li>\
+  </ul>";
+
+  res.send(html);
+});
+//Log out
+app.get('/logout', function(req, res){
+  console.log('logging out');
+  req.logout();
+  res.redirect('/');
+});
+
+app.get('/allow', function (req, res) {
+  var html = "<h1>Secrect area</h1>" + req.session.code;
+  res.send(html);
+});
+
 app.get('/auth/socrata', passport.authenticate('Socrata'));
 
-app.get('/auth/example/callback',
-  // passport.authenticate('oauth2', { failureRedirect: '/login' }),
-  function(req, res) {
+// const https = require('request');
+app.get('/auth/socrata/callback',
+  passport.authenticate('Socrata', { failureRedirect: '/' }),
+  function(req, res, next) {
     // Successful authentication, redirect home.
-    // res.redirect('/');
-    res.send('Hello World!')
+    req.session.code = req.query.code;
+    // res.redirect('/allow');
+    next();
+  });
+  app.post('/auth/access_token',
+  function(req, res, next) {
+    req.session.code = req.query.code;
+    res.redirect('/allow');
   });
 
 app.use('/', require('./routes/routes')(creds));
