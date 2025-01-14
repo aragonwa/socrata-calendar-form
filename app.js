4381223b01b85c744e17d0e0afec64e89b83b9bb
Alex Aragon <Alex.Aragon@kingcounty.gov>
2025-01-14 09:01:37 -0800

    init

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const mountRoutes = require('./routes');
-//Edit
 
 const app = express();
 
