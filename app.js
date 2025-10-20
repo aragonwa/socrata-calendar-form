d7b389eb5f8b51a9aeab236d5e36d45ab20399d5
Alex Aragon <Alex.Aragon@kingcounty.gov>
2025-10-20 09:27:38 -0700

    Adding cats to body

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ const app = express();
 if (process.env.NODE_ENV !== 'production') {
   app.use(require('morgan')('dev'));
 }
-const creds = (process.env.NODE_ENV === 'production') ? require('./config/config.prod.js') : require('./config/config.dev.js');
+const creds = (process.env.NODE_ENV === 'Production') ? require('./config/config.prod.js') : require('./config/config.dev.js');
 const port = process.env.PORT || 3000;
 
 app.use(session({
-  name: 'session',
-  keys: [creds.session.secret],
+  // name: 'session',
+  // keys: [creds.session.secret],
   secret: creds.session.secret,
   resave: false, // don't save session if unmodified
-  saveUninitialized: false, // don't create session until something stored,
+  saveUninitialized: true, // don't create session until something stored,
 }));
 
 app.set('view engine', 'pug');
--- util/processBody.js
+++ util/processBody.js
@@ -3,24 +3,32 @@ module.exports = (body) => {
   body.displayOnHomePage = (body.displayOnHomePage) ? true : false;
   body.categoryAirport = (body.categoryAirport) ? true : false;
   body.categoryArtsCulture = (body.categoryArtsCulture) ? true : false;
+  body.categoryBearCreekSammamish = (body.categoryBearCreekSammamish) ? true : false;
   body.categoryBestStarts = (body.categoryBestStarts) ? true : false;
   body.categoryBusiness = (body.categoryBusiness) ? true : false;
   body.categoryClosure = (body.categoryClosure) ? true : false;
   body.categoryCouncil = (body.categoryCouncil) ? true : false;
   body.categoryCourts = (body.categoryCourts) ? true : false;
+  body.categoryEastFederalWay = (body.categoryEastFederalWay) ? true : false;
+  body.categoryEastRentonPlateau = (body.categoryEastRentonPlateau) ? true : false;
   body.categoryElections = (body.categoryElections) ? true : false;
   body.categoryEmergency = (body.categoryEmergency) ? true : false;
   body.categoryEmployees = (body.categoryEmployees) ? true : false;
   body.categoryEnvironment = (body.categoryEnvironment) ? true : false;
   body.categoryEvents = (body.categoryEvents) ? true : false;
   body.categoryExecutive = (body.categoryExecutive) ? true : false;
+  body.categoryFairwoodCommunity = (body.categoryFairwoodCommunity) ? true : false;
   body.categoryFleetAuctions = (body.categoryFleetAuctions) ? true : false;
+  body.categoryFourCreeksTigerMountain = (body.categoryFourCreeksTigerMountain) ? true : false;
+  body.categoryGreaterMapleValleyCedarRiverArea = (body.categoryGreaterMapleValleyCedarRiverArea) ? true : false;
   body.categoryHealth = (body.categoryHealth) ? true : false;
   body.categoryJails = (body.categoryJails) ? true : false;
   body.categoryJobs = (body.categoryJobs) ? true : false;
   body.categoryLicenses = (body.categoryLicenses) ? true : false;
+  body.categoryLocalServices = (body.categoryLocalServices) ? true : false;
   body.categoryMetro = (body.categoryMetro) ? true : false;
   body.categoryNews = (body.categoryNews) ? true : false;
+  body.categoryNorthHighlineWhiteCenter = (body.categoryNorthHighlineWhiteCenter) ? true : false;
   body.categoryOperations = (body.categoryOperations) ? true : false;
   body.categoryParks = (body.categoryParks) ? true : false;
   body.categoryPermits = (body.categoryPermits) ? true : false;
@@ -28,6 +36,8 @@ module.exports = (body) => {
   body.categoryProperty = (body.categoryProperty) ? true : false;
   body.categoryRecreation = (body.categoryRecreation) ? true : false;
   body.categoryRecyclingTrash = (body.categoryRecyclingTrash) ? true : false;
+  body.categorySkywayWestHill = (body.categorySkywayWestHill) ? true : false;
+  body.categorySnoqualmieValleyNEKingCountyAreas = (body.categorySnoqualmieValley) ? true : false;
   body.categorySolidWasteHome = (body.categorySolidWasteHome) ? true : false;
   body.categorySolidWasteAdvisory = (body.categorySolidWasteAdvisory) ? true : false;
   body.categorySolidWasteEcoConsumer = (body.categorySolidWasteEcoConsumer) ? true : false;
@@ -35,12 +45,15 @@ module.exports = (body) => {
   body.categorySolidWasteGreenTools = (body.categorySolidWasteGreenTools) ? true : false;
   body.categorySolidWasteRecyclingEvents = (body.categorySolidWasteRecyclingEvents) ? true : false;
   body.categorySolidWasteWastemobile = (body.categorySolidWasteWastemobile) ? true : false;
+  body.categorySoutheastKingCounty = (body.categorySoutheastKingCounty) ? true : false;
   body.categoryRoads = (body.categoryRoads) ? true : false;
   body.categoryRural = (body.categoryRural) ? true : false;
   body.categorySafety = (body.categorySafety) ? true : false;
   body.categorySheriff = (body.categorySheriff) ? true : false;
   body.categorySocialServices = (body.categorySocialServices) ? true : false;
   body.categoryTransportation = (body.categoryTransportation) ? true : false;
+  body.categoryTAC = (body.categoryTAC) ? true : false;
+  body.categoryVashonMauryIsland = (body.categoryVashonMauryIsland) ? true : false;
   body.categoryVolunteer = (body.categoryVolunteer) ? true : false;
   body.categoryWaterTaxi = (body.categoryWaterTaxi) ? true : false;
   const data = {
@@ -66,24 +79,32 @@ module.exports = (body) => {
     display_on_kc_home_page: body.displayOnHomePage,
     airport: body.categoryAirport,
     arts_culture: body.categoryArtsCulture,
+    bear_creek_sammamish: body.categoryBearCreekSammamish,
     best_starts_for_kids: body.categoryBestStarts,
     business: body.categoryBusiness,
     closure: body.categoryClosure,
     council: body.categoryCouncil,
     courts: body.categoryCourts,
+    east_federal_way: body.categoryEastFederalWay,
+    east_renton_plateau: body.categoryEastRentonPlateau,
     elections: body.categoryElections,
     emergency: body.categoryEmergency,
     employees: body.categoryEmployees,
     environment: body.categoryEnvironment,
     events: body.categoryEvents,
     executive: body.categoryExecutive,
+    fairwood_community: body.categoryFairwoodCommunity,
     fleet_auctions: body.categoryFleetAuctions,
+    four_creeks_tiger_mountain: body.categoryFourCreeksTigerMountain,
+    greater_maple_valley_cedar_river_area: body.categoryGreaterMapleValleyCedarRiverArea,
     health: body.categoryHealth,
     jails: body.categoryJails,
     jobs: body.categoryJobs,
     licenses: body.categoryLicenses,
+    local_services: body.categoryLocalServices,
     metro: body.categoryMetro,
     news: body.categoryNews,
+    north_highline_white_center: body.categoryNorthHighlineWhiteCenter,
     operations: body.categoryOperations,
     parks: body.categoryParks,
     permits: body.categoryPermits,
@@ -91,6 +112,9 @@ module.exports = (body) => {
     property: body.categoryProperty,
     recreation: body.categoryRecreation,
     recycling_trash: body.categoryRecyclingTrash,
+    skyway_west_hill: body.categorySkywayWestHill,
+    snoqualmie_valley_ne_king_county_areas: body.categorySnoqualmieValleyNEKingCountyAreas,
+    southeast_king_county: body.categorySoutheastKingCounty,
     swd_home: body.categorySolidWasteHome,
     swd_advisory_committee: body.categorySolidWasteAdvisory,
     swd_ecoconsumer: body.categorySolidWasteEcoConsumer,
@@ -104,9 +128,10 @@ module.exports = (body) => {
     sheriff: body.categorySheriff,
     socialservices: body.categorySocialServices,
     transportation: body.categoryTransportation,
+    vashon_maury_island: body.categoryVashonMauryIsland,
     volunteer: body.categoryVolunteer,
     water_taxi: body.categoryWaterTaxi,
     tac_meeting: body.categoryTAC
   };
   return data;
-}
+};
